c10b18cd6d88ee1a0818e2239693e647e349761d
ef219ogozxoebo1lolzls114f <119745291+ef219ogozxoebo1lolzls114f@users.noreply.github.com>
2024-07-08 14:28:25 +0300

    Update unc.lua

--- unc.lua
+++ unc.lua
@@ -3,8 +3,3 @@ print("Tidal has been Injected!")
 getgenv().getexecutorname = function ()
 	return "Tidal V2"
 end
-
-getgenv().unc = function()
-	return "loadstring(game.HttpGet(game, "https://github.com/unified-naming-convention/NamingStandard/blob/main/UNCCheckEnv.lua"))();"
-end
-
